7c5e617fe14d57e022f72a6054a7bb8fed4ddd19
GabrielDias216 <bebel54321@icloud.com>
2024-05-02 08:54:36 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./**/*.{html,js}"];
+export const theme = {
+  fontFamily: {
+    'sans': ['Roboto', 'sans-serif']
+  },
+};
+export const plugins = [];
+   
+
